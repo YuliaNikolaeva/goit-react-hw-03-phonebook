7d2cdf938e4788a8873bd5fef391ddec0389a8f4
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2020-07-07 15:01:24 +0300

    fix hw-03 part 1

--- src/App.js
+++ src/App.js
@@ -91,7 +91,6 @@ export default class App extends Component {
                             onChange={this.changeFilter}
                         />
                     )}
-                    ;
                     <ContactForm OnSubmit={this.addContact} />
                 </Container>
 
